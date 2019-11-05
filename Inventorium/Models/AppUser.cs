952f13891d80f95eebe603b7098c5ebd59540b5e
Dave Ferguson <dave@gibberin.com>
2019-11-04 21:10:28 -0800

    Create AppUser.cs

---
+++ Inventorium/Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventorium.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public AppUser()
+        {
+
+        }
+
+        public string FullName() { return (FirstName + " " + LastName).Trim(); }
+    }
+}
